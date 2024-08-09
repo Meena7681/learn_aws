d4c40c8152dfc820b99c3f5a59972c20f50054cb
Manish Meena <manishmeena7681@gmail.com>
2024-08-09 17:41:43 +0000

    s3 with java  maven

---
+++ s3/sdk/java/myapp/src/main/java/com/example/myapp/DependencyFactory.java
@@ -0,0 +1,26 @@
+
+package com.example.myapp;
+
+import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
+import software.amazon.awssdk.http.crt.AwsCrtAsyncHttpClient;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3AsyncClient;
+
+/**
+ * The module containing all dependencies required by the {@link App}.
+ */
+public class DependencyFactory {
+
+    private DependencyFactory() {}
+
+    /**
+     * @return an instance of S3AsyncClient
+     */
+    public static S3AsyncClient s3Client() {
+        return S3AsyncClient.builder()
+                       .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
+                       .region(Region.AP_SOUTH_1)
+                       .httpClientBuilder(AwsCrtAsyncHttpClient.builder())
+                       .build();
+    }
+}
